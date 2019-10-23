0d0eca62074db3693152a13b73771fb56fbea5ab
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-10-23 13:02:06 -0700

    finished iterative sorts

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -1,11 +1,15 @@
 #!python
 
-
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    for i in range(len(items) - 1):
+
+        if(items[i] > items[i+1]):
+            return False
+
+    return True
 
 
 def bubble_sort(items):
@@ -13,8 +17,22 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    if(len(items) < 2):
+        return
+
+    is_bad = True
+    good_spots = 1
+    while is_bad:
+
+        swaps = 0
+        for i in range(len(items) - good_spots):
+
+            if(items[i] > items[i+1]):
+                items[i], items[i+1] = items[i+1], items[i]
+                swaps += 1
+
+        is_bad = swaps > 0
+        good_spots += 1
 
 
 def selection_sort(items):
@@ -22,9 +40,17 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    if(len(items) < 2 or is_sorted(items)):
+        return
+
+    for i in range(len(items) - 1):
+        min = i
+        for j in range(i, len(items)):
+
+            if(items[j] < items[min]):
+                min = j
+
+        items[min], items[i] = items[i], items[min]
 
 
 def insertion_sort(items):
@@ -32,6 +58,17 @@ def insertion_sort(items):
     order in front of items, and repeating until all items are in order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Take first unsorted item
-    # TODO: Insert it in sorted order in front of items
+    if(len(items) < 2 or is_sorted(items)):
+        return
+
+    for i in range(1, len(items)):
+
+        for j in range(0,i):
+
+            if(items[i - j] < items[i-j-1]):
+                items[i-j], items[i-j-1] = items[i-j-1], items[i-j]
+            else:
+                break
+
+
+#print(bubble_sort([1,5,3,6,8]))
